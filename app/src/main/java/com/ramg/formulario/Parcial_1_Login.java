e08655e8902b8e573b6f7ed8193c834497618cee
Medina <medinaverano17@gmail.com>
2024-02-26 14:49:08 +0100

    "Segunda versión del trabajo"

---
+++ app/src/main/java/com/ramg/formulario/Parcial_1_Login.java
@@ -0,0 +1,14 @@
+package com.ramg.formulario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Parcial_1_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parcial1_login);
+    }
+}
